366613306bf35f413b11726a5cae0c7cf5abf7ba
G0tem <bindirin@yahoo.com>
2025-01-10 00:02:56 +0500

    add backend auth and strategy

---
+++ app/authentification/strategy.py
@@ -0,0 +1,12 @@
+from fastapi import Depends
+from fastapi_users.authentication.strategy.db import AccessTokenDatabase, DatabaseStrategy
+
+from app.config import LIFETIME_SECONDS
+from app.database import get_access_token_db
+from models.AccessTokenModel import AccessToken
+
+
+def get_database_strategy(
+    access_token_db: AccessTokenDatabase[AccessToken] = Depends(get_access_token_db),
+) -> DatabaseStrategy:
+    return DatabaseStrategy(access_token_db, LIFETIME_SECONDS)
