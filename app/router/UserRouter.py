6b7e02ba55bf893bd38e027b9700e781785cfd2e
G0tem <bindirin@yahoo.com>
2025-01-17 19:49:04 +0500

    add router user me

--- app/router/UserRouter.py
+++ app/router/UserRouter.py
@@ -3,7 +3,7 @@ from fastapi import APIRouter
 from models.UserModel import User
 from authentification.user_manager import get_user_manager
 from authentification.backend import authentication_backend
-from schemas.UserSchemas import UserRead, UserCreate
+from schemas.UserSchemas import UserRead, UserCreate, UserUpdate
 
 
 fastapi_users = FastAPIUsers[User, int](
@@ -16,14 +16,20 @@ user_router = APIRouter(
     tags=["User"]
 )
 
-# login and logaut
+# /login and /logaut
 user_router.include_router(
     fastapi_users.get_auth_router(authentication_backend),
     prefix="/auth",
 )
 
-# register
+# /register
 user_router.include_router(
     fastapi_users.get_register_router(UserRead, UserCreate),
     prefix="/auth",
+)
+
+# /me and /id
+user_router.include_router(
+    fastapi_users.get_users_router(UserRead, UserUpdate),
+    prefix="/users",
 )
