ef55dd9ed2700e6b1eda104c41513d6257613a6e
G0tem <bindirin@yahoo.com>
2025-01-18 15:02:26 +0500

    delete print

--- app/authentification/user_manager.py
+++ app/authentification/user_manager.py
@@ -15,19 +15,16 @@ class UserManager(IntegerIDMixin, BaseUserManager[User, int]):
 
     async def on_after_register(self, user: User, request: Optional[Request] = None):
         log.warning("User %r has registered.", user.id)
-        print(f"User {user.id} has registered.")
 
     async def on_after_forgot_password(
         self, user: User, token: str, request: Optional[Request] = None
     ):
         log.warning("User %r has forgot their password. Reset token: %r", user.id, token)
-        print(f"User {user.id} has forgot their password. Reset token: {token}")
 
     async def on_after_request_verify(
         self, user: User, token: str, request: Optional[Request] = None
     ):
         log.warning("Verification requested for user %r. Verification token: %r", user.id, token)
-        print(f"Verification requested for user {user.id}. Verification token: {token}")
 
 
 async def get_user_manager(user_db=Depends(get_user_db)):
