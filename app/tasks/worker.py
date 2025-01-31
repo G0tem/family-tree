1776bb1468afb88e6a35028efb9dd8748e575c4d
G0tem <bindirin@yahoo.com>
2025-01-31 17:03:14 +0500

    create container app, fix addres

--- app/tasks/worker.py
+++ app/tasks/worker.py
@@ -2,7 +2,7 @@ from celery import Celery
 
 
 celery_app = Celery(
-    "worker", broker="redis://0.0.0.0:6379/0", backend="redis://0.0.0.0:6379/0"
+    "worker", broker="redis://redis:6379/0", backend="redis://redis:6379/0"
 )
 
 
