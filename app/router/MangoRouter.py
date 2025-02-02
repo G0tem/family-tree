096ef4598592c82a0e9e7a9f598df57e999c84f6
G0tem <bindirin@yahoo.com>
2025-02-02 22:35:25 +0500

    add docstring to router Mangodb

--- app/router/MangoRouter.py
+++ app/router/MangoRouter.py
@@ -9,6 +9,14 @@ mango_router = APIRouter(prefix="/api/v1", tags=["Mango"])
 
 @mango_router.post("/items/", response_model=Item)
 def create_item(item: Item):
+    """Create item
+
+    Args:
+        item (Item): Item model
+
+    Returns:
+        _type_: Item model save
+    """
     item_dict = item.dict()
     result = collection.insert_one(item_dict)
     item_dict["id"] = str(result.inserted_id)
@@ -18,6 +26,17 @@ def create_item(item: Item):
 
 @mango_router.get("/items/{item_id}", response_model=Item)
 def read_item(item_id: str):
+    """Get item
+
+    Args:
+        item_id (str): str id item in db
+
+    Raises:
+        HTTPException: 404 not found
+
+    Returns:
+        _type_: item from MangoDB
+    """
     item = collection.find_one({"_id": ObjectId(item_id)})
     if item is None:
         raise HTTPException(status_code=404, detail="Item not found")
@@ -27,6 +46,18 @@ def read_item(item_id: str):
 
 @mango_router.put("/items/{item_id}", response_model=Item)
 def update_item(item_id: str, item: Item):
+    """Update item
+
+    Args:
+        item_id (str): str id item in db
+        item (Item): Item model
+
+    Raises:
+        HTTPException: 404 not found
+
+    Returns:
+        _type_: Uptate item result
+    """
     item_dict = item.dict()
     result = collection.update_one({"_id": ObjectId(item_id)}, {"$set": item_dict})
     if result.matched_count == 0:
@@ -38,6 +69,17 @@ def update_item(item_id: str, item: Item):
 
 @mango_router.delete("/items/{item_id}")
 def delete_item(item_id: str):
+    """Delete item
+
+    Args:
+        item_id (str): item id
+
+    Raises:
+        HTTPException: 404 not found
+
+    Returns:
+        _type_: dict result delete
+    """
     result = collection.delete_one({"_id": ObjectId(item_id)})
     if result.deleted_count == 0:
         raise HTTPException(status_code=404, detail="Item not found")
